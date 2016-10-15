cfdaa2d9f2fb7da0915da3262333cbb6aaa5710d
Destructible-Watermelon <Halfling.wizard123@gmail.com>
2016-10-16 08:38:39 +1030

    Added hop, skip, jump, five-negative, and also moved the command dict out of the main loop, changed main loop to a while iterator, instead of for, to make control flow easier.

--- split.py
+++ split.py
@@ -3,13 +3,29 @@
 stack=[]
 program=[i.lower() for i in input().split()]
 pop=lambda:stack.pop() if stack else 0
-for command in program: #the part that will be the most extended
-    {
-        "five":         lambda: stack.append(5),
-        "five-unary":    lambda: (stack.append(0)for i in range(5)),
-        "divide":       lambda: stack.append(pop()//pop()),
-        "add":          lambda: stack.append(pop()+pop()),
-        "derp":         lambda: print("Derp" if input()=="derp" else "Nope"),
-        "derp-nocase":  lambda: print("Derp" if input().lower()=="derp" else "Nope"),
-    }.get(command, lambda:None)()
-print(' '.join(str(i) for i in stack))
+# commands that can't be lambdas
+def hop():
+    global i
+    if pop():
+        i+=1
+def skip():
+    global i; i+=1
+def jump():
+    global i; i-=pop()+1
+commands={
+    "five":          lambda: stack.append(5),
+    "five-negative": lambda: stack.append(-5),
+    "five-unary":    lambda: (stack.append(0)for i in range(5)),
+    "divide":        lambda: stack.append(pop()//pop()),
+    "add":           lambda: stack.append(pop()+pop()),
+    "derp":          lambda: print("Derp" if input()=="derp" else "Nope"),
+    "derp-nocase":   lambda: print("Derp" if input().lower()=="derp" else "Nope"),
+    "jump":          jump,
+    "skip":          skip,
+    "hop":           hop,
+    }
+i=0
+while i<len(program):
+    commands.get(program[i], lambda:None)()
+    i+=1
+if stack:print(' '.join(str(i) for i in stack))
